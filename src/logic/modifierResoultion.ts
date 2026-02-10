69e1d37de203a5979a0cf7bbfaa2abd01eddb3eb
Denmads <43139465+Denmads@users.noreply.github.com>
2026-02-10 22:33:30 +0100

    Fix modifier resolution to use rawValue for calculations

--- src/logic/modifierResoultion.ts
+++ src/logic/modifierResoultion.ts
@@ -4,10 +4,10 @@ export function resolveModifiers(base: number, mods: Modifier[]) {
   let value = base;
 
   mods.filter(m => m.type === "add")
-      .forEach(m => value += m.value);
+      .forEach(m => value += m.value.rawValue);
 
   mods.filter(m => m.type === "mul")
-      .forEach(m => value *= m.value);
+      .forEach(m => value *= m.value.rawValue);
 
   return Math.max(0, value);
 }
